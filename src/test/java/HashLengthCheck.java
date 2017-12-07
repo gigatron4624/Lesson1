97e914467e579c9f01660d08bd20bc2acbf3b279
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2017-12-06 20:30:32 -0600

    We're going tweaking!
    1. Added TestNG assertion methods.
    2. Inserted message to confirm assertion methods.

--- src/test/java/HashLengthCheck.java
+++ src/test/java/HashLengthCheck.java
@@ -1,15 +1,29 @@
+// This code uses TestNG to check if the lengths of the hash values are as expected when hashing different inputs.
+import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
+import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
+import static org.apache.commons.codec.digest.DigestUtils.sha384Hex;
+import static org.apache.commons.codec.digest.DigestUtils.sha512Hex;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
+//
 public class HashLengthCheck {
-    public String s = "Join the dark side!";
+    private String s = "Join the light side!"; // this is a test string
+    public String victory(int x) {
+        return "I got " + x + " bytes! Dilly Dilly!";
+    }
     @Test
     public void testHd() throws Exception {
-        int SHA1L = Lesson1.hexus1(s).length();
-        int SHA256L = Lesson1.hexus256(s).length();
-        int SHA384L = Lesson1.hexus384(s).length();
-        int SHA512L = Lesson1.hexus512(s).length();
+        int SHA1L = sha1Hex(s).length();
+        String M1 = victory(SHA1L);
+        Assert.assertTrue(SHA1L == 40, M1);
+        int SHA256L = sha256Hex(s).length();
+        String M256 = victory(SHA256L);
+        Assert.assertTrue(SHA256L == 64, M256);
+        int SHA384L = sha384Hex(s).length();
+        String M384 = victory(SHA384L);
+        Assert.assertTrue(SHA384L == 96, M384);
+        int SHA512L = sha512Hex(s).length();
+        String M512 = victory(SHA512L);
+        Assert.assertTrue(SHA512L == 128, M512);
     }
-
 }
