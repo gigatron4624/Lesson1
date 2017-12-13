fab63691162605edff0b1ffe8c3f79d3c1d0c085
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2017-12-13 16:57:02 -0600

    Finishing touches.
    Spaced out lines; added comments

--- src/test/java/org/gigatron4624/HashLengthCheck.java
+++ src/test/java/org/gigatron4624/HashLengthCheck.java
@@ -1,30 +1,48 @@
 package org.gigatron4624;
-// This code uses TestNG to check if the lengths of the hash values are as expected when hashing different inputs.
+/* This file uses TestNG to check if the lengths of the hash values are as expected when hashing different inputs.
+* TestNG is an upgrade compared to JUnit, the original Java unit testing framework.
+* The main method is optional for this test class and is thus excluded. */
+
 import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 import static org.apache.commons.codec.digest.DigestUtils.sha384Hex;
 import static org.apache.commons.codec.digest.DigestUtils.sha512Hex;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-//
+
 public class HashLengthCheck {
+
     private String s = "Join the light side!"; // this is a test string
 
-    public String woeisme(int x) { // This error message indicates that the test is unsuccessful.
-        return "I didn't get " + x + " bytes! Oh noes!"; // Successful tests don't print this error message.
+    /* WoeIsMe: error message indicating that the actual and expected lengths don't match */
+    public String WoeIsMe(int x) {
+        return "I didn't get " + x + " bytes! Oh noes!";
     }
     @Test // The test is right below this line.
-    public void testHd() throws Exception { // checks byte count (or length) of each hex string through TestNG
+
+    /* testHd: checks byte count (or length) of each hex string through TestNG assertion methods
+    * testHd throws an exception if the actual and expected lengths of any hex string do not match.
+    * If testHd doesn't throw an exception, the assertion methods pass */
+    public void testHd() throws Exception {
+
         int SHA1L = sha1Hex(s).length(); // length of SHA-1 hex string
-        Assert.assertTrue(SHA1L == 40, woeisme(SHA1L)); // asserts that the SHA-1 hex string takes 40 hex bytes
+
+        // asserts that the SHA-1 hex string takes 40 hex bytes
+        Assert.assertTrue(SHA1L == 40, WoeIsMe(SHA1L));
 
         int SHA256L = sha256Hex(s).length(); // length of SHA-256 hex string
-        Assert.assertTrue(SHA256L == 64, woeisme(SHA256L)); // asserts that the SHA-256 hex string takes 64 hex bytes
+
+        // asserts that the SHA-256 hex string takes 64 hex bytes
+        Assert.assertTrue(SHA256L == 64, WoeIsMe(SHA256L));
 
         int SHA384L = sha384Hex(s).length(); // length of SHA-384 hex string
-        Assert.assertTrue(SHA384L == 96, woeisme(SHA384L)); // asserts that the SHA-384 hex string takes 96 hex bytes
+
+        // asserts that the SHA-384 hex string takes 96 hex bytes
+        Assert.assertTrue(SHA384L == 96, WoeIsMe(SHA384L));
 
         int SHA512L = sha512Hex(s).length(); // length of SHA-512 hex string
-        Assert.assertTrue(SHA512L == 128, woeisme(SHA512L)); // asserts that the SHA-512 hex string takes 128 hex bytes
+
+        // asserts that the SHA-512 string takes 128 hex bytes
+        Assert.assertTrue(SHA512L == 128, WoeIsMe(SHA512L));
     }
 }
