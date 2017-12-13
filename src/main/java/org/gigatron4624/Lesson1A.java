381a3a1e4c6264a4fa1f29965d030d22a4015903
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2017-12-13 16:55:35 -0600

    Finishing touches.
    Added some lines of comments.

--- src/main/java/org/gigatron4624/Lesson1A.java
+++ src/main/java/org/gigatron4624/Lesson1A.java
@@ -1,5 +1,6 @@
 package org.gigatron4624;
-// This program simply checks if the secure hash algorithms yield the correct output for all input strings.
+/* This program simply checks if the secure hash algorithms yield the correct output for all input strings.
+* The program is now expendable since TestNG assertion methods validate lengths of secure hash algorithms. */
 
 import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
@@ -8,24 +9,32 @@ import static org.apache.commons.codec.digest.DigestUtils.sha512Hex;
 
 public class Lesson1A {
 
+    /* Private Methods */
+
+    /* checks if SHA-1 hash has 40 hexadecimal bytes, or 160 bits */
     private boolean SH1LC(String s){
-        return sha1Hex(s).length() == 40; // checks if SHA-1 hash has 40 hexadecimal bytes, or 160 bits
+        return sha1Hex(s).length() == 40;
     }
 
+    /* checks if SHA-1 hash has 64 hexadecimal bytes, or 256 bits */
     private boolean SH256LC(String s){
-        return sha256Hex(s).length() == 64; // checks if SHA-1 hash has 64 hexadecimal bytes, or 256 bits
+        return sha256Hex(s).length() == 64;
     }
 
+    /* checks if SHA-1 hash has 96 hexadecimal bytes, or 384 bits */
     private boolean SH384LC(String s){
-        return sha384Hex(s).length() == 96; // checks if SHA-1 hash has 96 hexadecimal bytes, or 384 bits
+        return sha384Hex(s).length() == 96;
     }
 
+    /* checks if SHA-1 hash has 128 hexadecimal bytes, or 512 bits */
     private boolean SH512LC(String s){
-        return sha512Hex(s).length() == 128; // checks if SHA-1 hash has 128 hexadecimal bytes, or 512 bits
+        return sha512Hex(s).length() == 128;
     }
 
-    // validates whether hex strings follow standard procedure
+    /* This method validates whether hex strings follow standard procedure. */
     private void HashCheck(String s){
+
+        // all four boolean methods must return true
         if (!SH1LC(s) || !SH256LC(s) || !SH384LC(s) || !SH512LC(s)) {
             System.out.println("Off to the Pit of Misery!"); // The algorithms don't work.
         }
@@ -34,20 +43,22 @@ public class Lesson1A {
         }
     }
 
+    /* Main Method */
+    /* The main method contains the sample strings and calls the HashCheck method. */
     public static void main(String[] args) {
 
         // pgm is short for program
         Lesson1A pgm = new Lesson1A(); // creates instance of class
 
-        /* Sample strings*/
+        /* sample strings*/
         String eins = "The cake is a lie!"; // eins is German for 1
         String zwei = "I like turtles!"; // zwei is German for 2
         String drei = "It's a me! Mario!"; // drei is German for 3
         String vier = "Game Over Yeah!!!!!!!!!!!"; // vier is German for 4
 
-        pgm.HashCheck(eins);
-        pgm.HashCheck(zwei);
-        pgm.HashCheck(drei);
-        pgm.HashCheck(vier);
+        pgm.HashCheck(eins); // runs first sample string through HashCheck
+        pgm.HashCheck(zwei); // runs second sample string through HashCheck
+        pgm.HashCheck(drei); // runs third sample string through HashCheck
+        pgm.HashCheck(vier); // runs last sample string through HashCheck
     }
 }
